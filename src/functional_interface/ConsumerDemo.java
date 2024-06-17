f39da435d65fcced82d2c01b079266dbda2c09d2
awanish <kumarawanish2024@gmail.com>
2024-06-17 11:06:27 +0530

    Updated Consumer Demo

--- src/functional_interface/ConsumerDemo.java
+++ src/functional_interface/ConsumerDemo.java
@@ -1,10 +1,16 @@
 package functional_interface;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerDemo {
     public static void main(String[] args) {
         Consumer<Integer> integerConsumer = System.out::println;
         integerConsumer.accept(5);
+
+        List<Integer> list = Arrays.asList(1, 2, 3, 6, 5, 7);
+        //Here forEach is accepting a consumer value
+        list.forEach(System.out::println);
     }
 }
