cb0dc50df6ef20258ff2075f7ebc941a81e9f128
awanish <kumarawanish2024@gmail.com>
2024-06-19 20:50:14 +0530

    Re-factored Stream Demo

--- src/java8_stream/EmployeeDemo.java
+++ src/java8_stream/EmployeeDemo.java
@@ -5,29 +5,26 @@ import java.util.stream.Collectors;
 
 public class EmployeeDemo {
 
-    public List<Employee> getTaxPayerEmployee(String arg){
-        EmployeeDao employeeDao = new EmployeeDao();
-
-        if(arg.equalsIgnoreCase("tax")){
-           return employeeDao.getAllEmployees().stream()
-                    .filter(employee -> employee.getSalary() >= 500000)
-                    .collect(Collectors.toList());
-        }else {
-            return employeeDao.getAllEmployees().stream()
-                    .filter(employee -> employee.getSalary() < 500000)
-                    .collect(Collectors.toList());
-        }
-    }
-
     public static void main(String[] args) {
         EmployeeDemo demo = new EmployeeDemo();
         System.out.println();
 
-        List<Employee> tax = demo.getTaxPayerEmployee("non tax");
-        for (Employee e : tax){
-            System.out.println("Id: "+e.getId()+" Name: "+e.getName()+" Salary: "+e.getSalary());
+        List<Employee> tax = demo.getTaxPayerEmployee("tax");
+        for (Employee e : tax) {
+            System.out.println("Id: " + e.getId() + " Name: " + e.getName() + " Salary: " + e.getSalary());
         }
 
+
+    }
+
+    public List<Employee> getTaxPayerEmployee(String arg) {
+        EmployeeDao employeeDao = new EmployeeDao();
+
+        return (arg.equalsIgnoreCase("tax")) ? employeeDao.getAllEmployees().stream()
+                .filter(employee -> employee.getSalary() >= 500000)
+                .collect(Collectors.toList()) : employeeDao.getAllEmployees().stream()
+                .filter(employee -> employee.getSalary() < 500000)
+                .collect(Collectors.toList());
 
     }
 }
