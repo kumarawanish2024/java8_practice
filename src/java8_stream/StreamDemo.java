e42faf16ab56c3e0948ca865a122e51ed8feb58d
awanish <kumarawanish2024@gmail.com>
2024-06-19 20:04:22 +0530

    Added code to Stream Demo

--- src/java8_stream/StreamDemo.java
+++ src/java8_stream/StreamDemo.java
@@ -1,8 +1,33 @@
 package java8_stream;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StreamDemo {
 
     public static void main(String[] args) {
-        StreamDemo demo = new StreamDemo();
+        List<String> nameList = Arrays.asList("Ram", "Mohan", "Sita", "Ravan", "Bali");
+
+        //using java7 interation
+        for (String name : nameList){
+            System.out.println(name);
+        }
+
+        //using index iteration
+        for (int i=0; i < nameList.size(); i++){
+            System.out.println(nameList.get(i));
+        }
+
+        //using foreach iteration
+        nameList.forEach(System.out::println);
+
+        //using stream
+        nameList.stream()
+                        .forEach(System.out::println);
+
+        //print name starting with character "M"
+        nameList.stream()
+                .filter(s -> s.startsWith("M"))
+                .forEach(System.out::println);
     }
 }
