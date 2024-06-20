40dd37d16758c42315ce253cbe9e337da3f7dc9a
awanish <kumarawanish2024@gmail.com>
2024-06-20 15:03:11 +0530

    Added sorting technique

--- src/java8_stream/EmployeeDemo.java
+++ src/java8_stream/EmployeeDemo.java
@@ -5,26 +5,33 @@ import java.util.stream.Collectors;
 
 public class EmployeeDemo {
 
+    EmployeeDao employeeDao = new EmployeeDao();
     public static void main(String[] args) {
         EmployeeDemo demo = new EmployeeDemo();
-        System.out.println();
 
         List<Employee> tax = demo.getTaxPayerEmployee("tax");
-        for (Employee e : tax) {
+        List<Employee> employees = demo.sortEmployeeBasedOnSalary(tax);
+        for (Employee e : employees) {
             System.out.println("Id: " + e.getId() + " Name: " + e.getName() + " Salary: " + e.getSalary());
         }
-
-
     }
 
     public List<Employee> getTaxPayerEmployee(String arg) {
-        EmployeeDao employeeDao = new EmployeeDao();
 
-        return (arg.equalsIgnoreCase("tax")) ? employeeDao.getAllEmployees().stream()
+        return (arg.equalsIgnoreCase("non tax")) ? employeeDao.getAllEmployees().stream()
                 .filter(employee -> employee.getSalary() >= 500000)
                 .collect(Collectors.toList()) : employeeDao.getAllEmployees().stream()
                 .filter(employee -> employee.getSalary() < 500000)
                 .collect(Collectors.toList());
 
     }
+
+    public List<Employee> sortEmployeeBasedOnSalary(List<Employee> list){
+
+        return list.stream()
+                .sorted((o1, o2) -> (int) (o2.getSalary() - o1.getSalary()))
+                .collect(Collectors.toList());
+    }
+
+
 }
